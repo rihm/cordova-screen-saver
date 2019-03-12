c5ec26c6e9713de2adeca12ac8842fcc8df751bd
rihm <rihm92@gmail.com>
2019-03-12 17:38:29 +0530

    Update screensavar.js

--- www/screensavar.js
+++ www/screensavar.js
@@ -1,1 +1,10 @@
 
+function setScreenSaver() {
+   
+    cordova.exec(setSuccess, setFailure, "Screensaver", "setScreenSaver");
+}
+
+function unSetScreenSaver() {
+    
+    cordova.exec(unSetSuccess, unSetFailure, "Screensaver", "unSetScreenSaver");
+}
